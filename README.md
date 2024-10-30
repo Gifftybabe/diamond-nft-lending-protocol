c36666e6133cc933e3b7e9d158f311611460b54d
Gifftybabe <ulokangozi@gmail.com>
2024-10-30 14:18:53 +0100

    updated code

--- README.md
+++ README.md
@@ -1,13 +1,39 @@
-# Sample Hardhat Project
+[![Mentioned in Awesome Foundry](https://awesome.re/mentioned-badge-flat.svg)](https://github.com/crisgarner/awesome-foundry)
+# Foundry + Hardhat Diamonds
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+This is a mimimal template for [Diamonds](https://github.com/ethereum/EIPs/issues/2535) which allows facet selectors to be generated on the go in solidity tests!
 
-Try running some of the following tasks:
+## Installation
 
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat ignition deploy ./ignition/modules/Lock.ts
+- Clone this repo
+- Install dependencies
+
+```bash
+$ yarn && forge update
 ```
+
+### Compile
+
+```bash
+$ npx hardhat compile
+```
+
+## Deployment
+
+### Hardhat
+
+```bash
+$ npx hardhat run scripts/deploy.js
+```
+
+### Foundry
+
+```bash
+$ forge t
+```
+
+`Note`: A lot of improvements are still needed so contributions are welcome!!
+
+Bonus: The [DiamondLoupefacet](contracts/facets/DiamondLoupeFacet.sol) uses an updated [LibDiamond](contracts/libraries//LibDiamond.sol) which utilises solidity custom errors to make debugging easier especially when upgrading diamonds. Take it for a spin!!
+
+Need some more clarity? message me [on twitter](https://twitter.com/Timidan_x), Or join the [EIP-2535 Diamonds Discord server](https://discord.gg/kQewPw2)
---
+++ contracts/facets/LoanFacet.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {LibDiamond} from "../libraries/LibDiamond.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract LoanFacet {
+    event LoanCreated(
+        uint256 indexed loanId,
+        address indexed _borrower,
+        address _currency,
+        uint256 _duration,
+        uint256 _dueDate,
+        uint256 _amount,
+        uint256 _interestRate,
+        address _collateral,
+        uint256 _collateralTokenId
+    );
+
+    //Borrower ask for loan by specifying loan terms
+    function createLoanTerms(
+        address _currency,
+        uint256 _duration,
+        // uint256 _dueDate,
+        uint256 _amount,
+        uint256 _interestRate,
+        address _collateral,
+        uint256 _collateralTokenId
+    ) external {
+        require(_currency != address(0), "Currency address cannot be zero");
+        require(_duration > 0, "Invalid duration");
+        require(_amount > 0, "Invalid amount");
+        require(
+            _interestRate >= 0 && _interestRate <= 100,
+            "Invalid interest rate"
+        );
+        require(_collateral != address(0), "Collateral address cannot be zero");
+        require(_collateralTokenId > 0, "Invalid collateral token Id");
+
+        // Check that the borrower owns the NFT collateral
+        IERC721 collateralToken = IERC721(_collateral);
+        require(
+            collateralToken.ownerOf(_collateralTokenId) == msg.sender,
+            "LoanFacet: Borrower does not own the NFT"
+        );
+
+        // Transfer NFT from borrower to contract for escrow
+        collateralToken.safeTransferFrom(
+            msg.sender,
+            address(this),
+            _collateralTokenId
+        );
+
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        ds.loanCount++;
+        uint256 currentLoanId = ds.loanCount;
+
+        ds.loanIdToLoan[currentLoanId] = LibDiamond.Loan({
+            loanId: currentLoanId,
+            borrower: msg.sender,
+            currency: _currency,
+            loanDuration: _duration,
+            dueDate: block.timestamp + _duration,
+            isRepaid: false,
+            amount: _amount,
+            interestRate: _interestRate, //In percent
+            collateral: _collateral,
+            collateralTokenId: _collateralTokenId,
+            status: LibDiamond.LoanStatus.Pending,
+            lender: address(0),
+            acceptedAt: 0,
+            paidAt: 0
+        });
+
+        ds.BorrowerToLoanId[msg.sender].push(currentLoanId);
+
+
+        emit LoanCreated(
+            currentLoanId,
+            msg.sender,
+            _currency,
+            _duration,
+            block.timestamp + _duration,
+            _amount,
+            _interestRate,
+            _collateral,
+            _collateralTokenId
+        );
+    }
+
+    //Get Loan
+    function getLoan(
+        uint256 _loanId
+    ) external view returns (LibDiamond.Loan memory) {
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        LibDiamond.Loan storage loan = ds.loanIdToLoan[_loanId];
+        require(loan.borrower != address(0), "Loan does not exist");
+        return loan;
+    }
+}
